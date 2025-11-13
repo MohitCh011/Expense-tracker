44e6a7b77643e276bf38ba1635fc0b504734f6fc
MohitCh011 <chmohit00@gmail.com>
2025-11-14 00:25:19 +0530

    Configure for production deployment

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-
+require('dotenv').config();
 dotenv.config();
 connectDB();
 
 const app = express();
-
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
